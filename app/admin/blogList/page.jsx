33093529c5fa4ba82c9657596f057ab1ea970daf
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2024-09-10 02:06:28 +0530

    final

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import BlogTableItem from '@/Components/AdminComponents/BlogTableItem'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import { assets, blog_data } from "@/Assets/assets";
 import Image from 'next/image'
