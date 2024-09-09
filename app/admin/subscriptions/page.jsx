cc843ac703a757cc0e1a422a03d9eba2d2966e78
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2024-09-10 02:37:24 +0530

    five

--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -1,63 +1,13 @@
 "use client";
-import SubsTableItem from '@/Components/AdminComponents/SubsTableItem'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { toast } from 'react-toastify'
+import React from 'react'
+import SubsProduct from '@/Components/Front/SubsProduct';
 
-const page = () => {
-    
-    const [emails, setEmails] = useState([]);
-
-    const fetchEmails = async() => {
-        const response = await axios.get('/api/email');
-        setEmails(response.data.emails);
-    }
-
-    const deleteEmail = async(mongoId) => {
-        const response = await axios.delete('/api/email', {
-            params: {
-                id: mongoId
-            }
-        })
-        if(response.data.success) {
-            toast.success(response.data.msg);
-            fetchEmails();
-        }
-        else {
-            toast.error("Error");
-        }
-    }
-
-    useEffect(() => {
-        fetchEmails();
-    }, [])
+const page = () => {    
 
     return (
-        <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
-            <h1>All Emails</h1>
-            <div className='relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
-                <table className='w-full text-small text-gray-500'>
-                    <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50'>
-                        <tr>
-                            <th scope='col' className='px-6 py-3'>
-                                Admin List
-                            </th>
-                            <th scope='col' className='hidden sm:block px-6 py-3'>
-                                Date 
-                            </th>
-                            {/* <th scope='col' className='px-6 py-3'>
-                                Action
-                            </th> */}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {emails.map((item, index)=> {
-                            return <SubsTableItem key={index} deleteEmail={deleteEmail} mongoId={item._id} email={item.email} date={item.date} /> 
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        <>
+            <SubsProduct />
+        </>
     )
 }
 
