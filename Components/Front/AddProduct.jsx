17109188602fda39a95f7ed0dd474ea9960db840
Abhishek Harsh <abhishekharsh3411@gmail.com>
2024-09-10 19:28:45 +0530

    Update AddProduct.jsx

--- Components/Front/AddProduct.jsx
+++ Components/Front/AddProduct.jsx
@@ -50,26 +50,26 @@ const AddProduct = () => {
 
     return (
         <>
-            <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
-                <p className='text-xl'>
+            <form onSubmit={onSubmitHandler} className='apone'>
+                <p className='aptwo'>
                     Upload thumbnail
                 </p>
                 <label htmlFor="image">
-                    <Image className='mt-4' src={!image ? assets.upload_area : URL.createObjectURL(image)} width={140} height={140} alt=''/>
+                    <Image className='apthr' src={!image ? assets.upload_area : URL.createObjectURL(image)} width={140} height={140} alt=''/>
                 </label>
                 <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden required/>
-                <p className='text-xl mt-4'>Blog title</p>
-                <input name='title' onChange={onChangeHandler} value={data.title} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder="Type Here" required />
+                <p className='apfour'>Blog title</p>
+                <input name='title' onChange={onChangeHandler} value={data.title} className='apfive' type="text" placeholder="Type Here" required />
                 <p className='text-xl mt-4'>Blog Description</p>
-                <textarea name='description' onChange={onChangeHandler} value={data.description} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder="Write Content Herer" rows={6} required />
+                <textarea name='description' onChange={onChangeHandler} value={data.description} className='apsix' type="text" placeholder="Write Content Here" rows={6} required />
                 <p className='text-xl mt-4'>Blog Category</p>
-                <select name="category" onChange={onChangeHandler} value={data.category} className="w-40 mt-4 px-4 py-3 border text-gray-500">
+                <select name="category" onChange={onChangeHandler} value={data.category} className="apsev">
                     <option value="Startup">Startup</option>
                     <option value="Technology">Technology</option>
                     <option value="Lifestyle">Lifestyle</option>
                 </select>
                 <br />
-                <button type="submit" className='mt-8 w-40 h-12 bg-black text-white'>ADD</button>
+                <button type="submit" className='apeig'>ADD</button>
             </form>
         </>
     )
